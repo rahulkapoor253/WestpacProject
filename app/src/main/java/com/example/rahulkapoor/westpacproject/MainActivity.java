9dc239f54e42fc4fd096becf999e1a66a987f5f2
Rahul Kapoor <rahul253801@gmail.com>
2019-05-19 20:37:05 +0530

    setting up multiple images picker and gridview & adapter

--- app/src/main/java/com/example/rahulkapoor/westpacproject/MainActivity.java
+++ app/src/main/java/com/example/rahulkapoor/westpacproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rahulkapoor.westpacproject;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -11,9 +12,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private Intent galleryIntent;
     private static int IMG_RESULT = 1;
     private String ImageDecode;
-    private ImageView imageViewLoad;
     private Bitmap bmp;
     BitmapFactory.Options options;
-
+    private ArrayList<String> imagesList = new ArrayList<>();
+    private GalleryAdapter galleryAdapter;
+    private GridView gvGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +37,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnGallery = (Button) findViewById(R.id.btn_opengallery);
-        imageViewLoad = (ImageView) findViewById(R.id.imageView1);
 
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
                 //open up gallery and select images;
-                galleryIntent = new Intent(Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                // galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
 
                 startActivityForResult(Intent.createChooser(galleryIntent, "Select picture"), IMG_RESULT);
@@ -56,33 +63,74 @@ public class MainActivity extends AppCompatActivity {
 
         try {
 
-            Log.d("gallery", data.getData().toString());
+            // Log.d("gallery", data.getData() + "");
+
 
             if (requestCode == IMG_RESULT && resultCode == RESULT_OK
-                    && data != null) {
+                    && null != data) {
 
-                final Uri URI = data.getData();
                 String[] FILE = {MediaStore.Images.Media.DATA};
+                imagesList = new ArrayList<String>();
 
-                Cursor cursor = getContentResolver().query(URI,
-                        FILE, null, null, null);
+                if (data.getData() != null) {
 
-                cursor.moveToFirst();
+                    Log.d("gallery", data.getData() + "");
 
-                int columnIndex = cursor.getColumnIndex(FILE[0]);
-                ImageDecode = cursor.getString(columnIndex);
-                cursor.close();
+                    final Uri URI = data.getData();
+                    // String[] FILE = {MediaStore.Images.Media.DATA};
 
-                Log.d("image path", ImageDecode);
+                    Cursor cursor = getContentResolver().query(URI,
+                            FILE, null, null, null);
 
-                findViewById(R.id.imageView1).post(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((ImageView) findViewById(R.id.imageView1)).setImageURI(URI);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(FILE[0]);
+                    ImageDecode = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    Log.d("image path", ImageDecode);
+
+                    ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                    mArrayUri.add(URI);
+                    galleryAdapter = new GalleryAdapter(getApplicationContext(), mArrayUri);
+                    gvGallery.setAdapter(galleryAdapter);
+                    gvGallery.setVerticalSpacing(gvGallery.getHorizontalSpacing());
+                    ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) gvGallery
+                            .getLayoutParams();
+                    mlp.setMargins(0, gvGallery.getHorizontalSpacing(), 0, 0);
+
+                } else {
+
+                    if (data.getClipData() != null) {
+//when multiple images are selected;
+                        Log.d("gallery", data.getClipData().getItemCount() + "");
+
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            mArrayUri.add(uri);
+                            // Get the cursor
+                            Cursor cursor = getContentResolver().query(uri, FILE, null, null, null);
+                            // Move to first row
+                            cursor.moveToFirst();
+
+                            galleryAdapter = new GalleryAdapter(getApplicationContext(), mArrayUri);
+                            gvGallery.setAdapter(galleryAdapter);
+                            gvGallery.setVerticalSpacing(gvGallery.getHorizontalSpacing());
+                            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) gvGallery
+                                    .getLayoutParams();
+                            mlp.setMargins(0, gvGallery.getHorizontalSpacing(), 0, 0);
+                        }
+
                     }
-                });
 
+                }
             }
+
         } catch (Exception e) {
             Toast.makeText(this, "Please try again", Toast.LENGTH_LONG)
                     .show();
